1196b318aad4004a292fc609504208b4ceb2be01
Nozima <nozimaqodirova07@gmail.com>
2025-02-07 18:39:28 +0500

    wishlist

--- src/components/wisherror/Wisherror.jsx
+++ src/components/wisherror/Wisherror.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import error from '../../assets/heart.png'
 
 const Wisherror = () => {
   return (
     <div className='flex flex-col items-center min-h-[400px] pt-32'>
-        <img className='w-[200px]' src="https://uzum.uz/static/img/hearts.cf414be.png" alt="" />
-        <h2>Empty</h2>
+        <img className='w-[200px]' src={error} alt="" />
+        <h2 className='text-2xl font-bold text-slate-700'>Empty</h2>
     </div>
   )
 }
